067912779c06b19a24f105d086cd5ca3e33b75f1
Amol Jagtap <amoljagtap17@gmail.com>
2021-10-01 19:45:27 +0530

    microfrontend_demo_app: integrate cart in container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
       name: 'container', // name is not used in host module. Only needed for remotes. Usual convention to add 'name'.
       remotes: {
         products: 'products@http://localhost:8081/remoteEntry.js',
+        cart: 'cart@http://localhost:8082/remoteEntry.js',
       },
     }),
     new HtmlWebpackPlugin({

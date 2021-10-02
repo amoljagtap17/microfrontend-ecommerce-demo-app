e78d10368e9448dada48ae67f658d5a2eee2efe8
Amol Jagtap <amoljagtap17@gmail.com>
2021-10-02 12:35:20 +0530

    microfrontend_demo_app: situations sub/app execution context

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -11,3 +11,15 @@ for (let i = 0; i < 5; i++) {
 // console.log('products : ', products)
 
 document.querySelector('#dev-products').innerHTML = products
+
+// Context/ Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// which definitely had an id 'dev-products'
+// we want to immediately render our app in that element
+
+// Context/ Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// we DO NOT WANT to immediately render the app

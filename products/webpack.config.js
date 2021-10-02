80cddb878e443ff8ac7420dbcb248805fc6aca77
Amol Jagtap <amoljagtap17@gmail.com>
2021-10-02 13:11:31 +0530

    microfrontend_demo_app: consume remote modules

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js',
       exposes: {
-        './ProductsIndex': './src/index',
+        // './ProductsIndex': './src/index',
+        './ProductsIndex': './src/bootstrap',
       },
       // shared: ['faker'],
       shared: {
